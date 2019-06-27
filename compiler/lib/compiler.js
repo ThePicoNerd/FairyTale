07e141e0a3983c46154cb7530e6ddbd0099845ff
Åke Amcoff <ake.amcoff@lynx.agency>
2019-06-27 14:44:02 +0200

    :)

--- compiler/lib/compiler.js
+++ compiler/lib/compiler.js
@@ -1,5 +1,20 @@
+const fs = require("fs-extra");
+
 function compile({source, target, author}) {
-  
+  let input = fs.readFileSync(source, "utf8");
+  let output = fs.createWriteStream(target);
+
+  // Cleanup
+  input = input.replace(/[ ](?=(?:(?:[^"]*"){2})*[^"]*$)/g, " ");
+  let lines = input.split("\n");
+
+  for (let lineNumber in lines) {
+    let line = lines[lineNumber];
+
+    output.write(line);
+
+    console.log(line);
+  }
 }
 
-module.exports = compile;
+exports.compile = compile;
